16f5d47e43c39474713fe6668cb17ab07cc264cc
Subash <91050840+Subash-Gopal@users.noreply.github.com>
2022-04-22 11:30:52 +0530

    Update testBatteryValueReceiver.cpp

--- Receiver/testBatteryValueReceiver.cpp
+++ Receiver/testBatteryValueReceiver.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN
 #include "test/catch.hpp"
-#include "Receiver/Receiver.cpp"
+#include "Receiver.cpp"
 
 TEST_CASE("Fetches data from a sensor and writtens to CSV FilE and tests the values reading from the CSV file ") {
   bool expectedOutput = true;
