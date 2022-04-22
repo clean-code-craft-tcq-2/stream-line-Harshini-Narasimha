f32ff19ab36cd0b6057b9273a6b08cc1e6c1566e
Subash <91050840+Subash-Gopal@users.noreply.github.com>
2022-04-22 11:27:37 +0530

    Update testBatteryValueReceiver.cpp

--- Receiver/testBatteryValueReceiver.cpp
+++ Receiver/testBatteryValueReceiver.cpp
@@ -1,5 +1,5 @@
 #define CATCH_CONFIG_MAIN
-#include "Receiver/test/catch.hpp"
+#include "test/catch.hpp"
 #include "Receiver/Receiver.cpp"
 
 TEST_CASE("Fetches data from a sensor and writtens to CSV FilE and tests the values reading from the CSV file ") {
